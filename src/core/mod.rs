4ff8afcf9f849baf6aceec9a153699e6a7cfb52f
xshadowlegendx <lyheng0@gmail.com>
2023-10-08 10:05:39 +0700

    un-export unnecessary enum

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -33,7 +33,7 @@ use super::AuthenticationFlow;
 
 pub use self::jwk::{
     CoreHmacKey, CoreJsonWebKey, CoreJsonWebKeyType, CoreJsonWebKeyUse, CoreRsaPrivateSigningKey,
-    CoreEdDsaKeyVariant, CoreEdDsaPrivateSigningKey
+    CoreEdDsaPrivateSigningKey
 };
 
 mod crypto;
