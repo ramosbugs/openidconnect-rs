005fc77e959284d157076fa9ed32f3549b8eb3bb
Graham Christensen <graham@grahamc.com>
2026-04-03 14:46:08 -0400

    JsonWebKey: add + Eq for the HashSet

--- src/types/jwk.rs
+++ src/types/jwk.rs
@@ -13,7 +13,7 @@ new_type![
 ];
 
 /// JSON Web Key.
-pub trait JsonWebKey: Clone + Debug + Hash + DeserializeOwned + Serialize + 'static {
+pub trait JsonWebKey: Clone + Debug + Eq + Hash + DeserializeOwned + Serialize + 'static {
     /// Allowed key usage.
     type KeyUse: JsonWebKeyUse;
 
